1a1b01bf3051679e358adb0fc1332a72e9686807
strongkid21 <1751862489@qq.com>
2025-04-29 12:49:02 +0800

    first

---
+++ dataset/fire/create_list.py
@@ -0,0 +1,30 @@
+import random
+import os
+
+random.seed(100)
+data_root_dir = "fireOrigin"
+annotate_root_dir = "annotations"
+image_root_dir = "images"
+
+itemList = []
+for ip, ap in zip(os.listdir(os.path.join(data_root_dir, image_root_dir)), os.listdir(
+        os.path.join(data_root_dir, annotate_root_dir))):
+    print(ap)
+    itemList.append((os.path.join(data_root_dir, image_root_dir, ip), os.path.join(data_root_dir, annotate_root_dir, ap)))
+
+ratio = 0.9
+random.shuffle(itemList)
+trainf = open(os.path.join("trainval.txt"), "w")
+testf = open(os.path.join("test.txt"), "w")
+
+for i, item in enumerate(itemList):
+    img, annot = item[0], item[1]
+    writeText = img + ' ' + annot + '\n'
+
+    if i < ratio * len(itemList):
+        trainf.write(writeText)
+    else:
+        testf.write(writeText)
+
+trainf.close()
+testf.close()
